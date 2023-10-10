5ca41ad962a97d5d6962b2a0dcc9e9ad635a4007
Mica <micaelavaleriasuarez@gmail.com>
2023-10-09 22:01:12 -0300

    Base

--- src/main/java/model/Foto.java
+++ src/main/java/model/Foto.java
@@ -1,6 +1,11 @@
 package model;
 
 public class Foto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@PrimaryKeyJoinColumn
+	@Column(name="id", nullable=false)
+	private int numero;
 
 	public Foto() {
 		// TODO Auto-generated constructor stub
